be70a37261d1ef223fecec9b4428c24d7144bfaa
William Gyasi <gyasi.wk@gmail.com>
2022-10-06 12:13:35 +0000

    SOLVING PUSH ISSUES

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PokemonThumb from "./Components/PokemonThumb";
+
+const BASEURL = `https://pokeapi.co/api/v2/pokemon`;
 
 function App() {
   const [monsters, setMonsters] = useState([]);
 
+  async function getPokemonDetails(singlePokemon) {
+    let data=[]
+    const res = await fetch(`${BASEURL}/${singlePokemon.name}`);
+    const resResults = await res.json();
+
+    const pokemonData = {
+      pokemon: singlePokemon.name,
+    };
+
+    
+
+    setMonsters(data)
+    
+  }
+
+  async function getPokemonList(callback) {
+    let monster = [];
+    const response = await fetch(`${BASEURL}?limit=20`);
+    const resResults = await response.json();
+    const { results } = resResults;
+    if (results.length > 0) {
+      results.map(getPokemonDetails);
+    } else {
+      console.log("NO POKEMON FOUND");
+    }
+  }
+
+  useEffect(() => {
+    getPokemonList();
+  }, []);
+
   return (
     <div className="app">
-      <div className="monstercontainer">
+      <div className="monsterscontainer">
         <div className="monsterscollection">
           {monsters.map((monster) => {
-            return (
-              <PokemonThumb monsterObj={monster} key={monster.monstername} />
-            );
+            console.log(monsters)
+            return <PokemonThumb monsterObj={monster} key={monster.pokemon} />;
           })}
         </div>
       </div>
--- src/Components/PokemonThumb.jsx
+++ src/Components/PokemonThumb.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 
-function PokemonThumb({monsterObj}) {
+function PokemonThumb({ monsterObj }) {
   return (
-  <div style={{
-    height:200,
-    width:200,
-    backgroundColor:"red",
-    marginRight:10,
-    display:"flex",
-    justifyContent:"center",
-    alignItems:"center",
-    flexDirection:"column",
-    borderRadius:"10px",
-    marginBottom:"10px"
-  }}>
-    {monsterObj.monstername}
-  </div>);
+    <div
+      style={{
+        height: 300,
+        width: 200,
+        backgroundColor: "red",
+        marginRight: 10,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        borderRadius: "10px",
+        marginBottom: "10px",
+      }}
+    >
+      <h4>{monsterObj.pokemon.toUpperCase()}</h4>
+    </div>
+  );
 }
 
 export default PokemonThumb;
